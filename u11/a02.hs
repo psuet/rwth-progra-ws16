a814bd7403c410eecf0a0103a3d54d9f7d1ddfc1
Ben <benimus@gmx.de>
2017-02-01 19:06:43 +0100

    Funktionierende b) ergänzt

--- u11/a02.hs
+++ u11/a02.hs
@@ -13,29 +13,29 @@ t1 = IndexKnoten 3 42 [IndexKnoten 3 15 [DatenKnoten 3, DatenKnoten 11, DatenKno
 					  ]
 --b)
 verzweigunggsgrad :: MulTree a -> Int
-verzweigunggsgrad (DatenKnoten _) =  0
-verzweigunggsgrad (IndexKnoten _ _ xs) = countList xs
-		where
-			countList :: [MulTree a] -> Int
-			countList [] = 0
-			countList (x:xs) = 1 + verzweigunggsgrad x + countList xs --gibt noch lediglich Gesamtverzweigung und nicht Maximalverzweigung an.
-
---b)
-verzweigunggsgrad :: MulTree a -> Int
-verzweigunggsgrad (DatenKnoten _) = []
-verzweigunggsgrad (IndexKnoten _ _ xs) = maxInt (makeList xs)
+verzweigunggsgrad (DatenKnoten _) = 0
+verzweigunggsgrad (IndexKnoten _ _ xs) = maxInteger (countList xs : makeList xs)
 		where
 			makeList :: [MulTree a] -> [Int]
 			makeList [] = []
-			makeList (x:xs) = (countList x) : [] ++ makeList xs 
+			makeList (x:xs) = countList xs : verzweigunggsgrad x : makeList xs
 
-			countList :: MulTree a -> Int
-			countList (DatenKnoten _) = 0
-			countList (IndexKnoten _ _ (x:xs)) = 1 + countList x
+			countList :: [MulTree a] -> Int
+			countList []  = 0
+			countList (x:xs) = 1 + countList xs
 
 			maxInteger :: [Int] -> Int
 			maxInteger [] = 0
 			maxInteger (x:xs) = if x > (maxInteger xs) then x else maxInteger xs
+
+--c)
+datenListe :: MulTree a -> [a]
+datenListe (DatenKnoten a) = a:[]
+datenListe (IndexKnoten a b xs) = a:b:iterateList xs
+		where
+			iterateList :: [MulTree a] -> [a]
+			iterateList [] = []
+			iterateList (x:xs) = datenListe x ++ iterateList xs
 {--d)
 datenIntervalle :: MulTree Int ->-}
 
