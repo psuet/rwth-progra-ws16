0e65055130012fb171244fc95ae94c6d6d94c56a
Richard Reiber <richard.reiber@gmail.com>
2017-02-01 11:54:38 +0100

    First steps for A8

--- u11/a8.hs
+++ u11/a8.hs
@@ -9,3 +9,21 @@ dropall (x : xs) = x : dropall (drop_mult x xs)
 
 primes :: [Int]
 primes = dropall (from 2)
+
+firstElement :: [Int] -> Int
+firstElement [] = 0
+firstElement (x : xs) = x
+
+nextElement :: Int -> [Int] -> Int
+nextElement _ [x] = 0
+nextElement i (x : xs) =  if (x == i) then firstElement xs else nextElement i xs
+
+previousElement :: Int -> [Int] -> Int
+previousElement _ [x] = 0
+previousElement i (x : xs) =  if ((firstElement xs) == i) then x else previousElement i xs
+
+-- Ist zwar logisch, funktioniert aber nicht!
+-- primes, nextElement und previousElement tun was sie sollen
+-- Problem ist also irgendwie der Filter!
+goodPrimes :: [Int]
+goodPrimes = filter (\x -> x * x > (previousElement x primes) * (nextElement x primes)) primes
