dc62004bb14adb7bf8a0b975e57d50add23e2b7e
Richard Reiber <richard.reiber@gmail.com>
2017-02-03 09:05:18 +0100

    A6 a), c)

--- u11/a06.hs
+++ u11/a06.hs
@@ -10,22 +10,24 @@ blist = Cons 1 (Cons 1 (Cons 0 (Cons 0 Nil)))
 -- Teilaufgabe a)
 filterList :: (a -> Bool) -> List a -> List a
 filterList f Nil = Nil
-filterList f (Cons a b) = if f then Cons a ++ filterList f b else filterList f b
+filterList f (Cons a b) = if f a then a `Cons` filterList f b else filterList f b
 
 -- Teilaufgabe b)
 divisibleBy :: Int -> List Int -> List Int
 divisibleBy n l = filterList (\x -> x `mod` n == 0) l
 
 -- Teilaufgabe c)
--- Possible Solution
---foldList :: (a-> b -> b) -> b -> List a -> b
---foldList f c Nil = c
---foldList f c (Cons a b) = f v (foldList f c b)
+foldList :: (a -> b -> b) -> b -> List a -> b
+foldList f c (Cons a Nil) = f a c
+foldList f c (Cons a b) = f a (foldList f c b)
+
+-- Testfunktion c)
+plus :: Int -> Int -> Int
+plus x y = x + y
 
 -- Teilaufgabe d)
 listMaximum :: List Int -> Int
 listMaximum Nil = minBound
---listMaximum (Cons v l) = foldList 
 
 --Teilaufgabe e)
 --zipLists :: (a-> b -> c) -> List a -> List b -> List c
