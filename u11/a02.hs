0e914b5e3cc84f1bc853365f029d21d0b6f8c9d4
berndtorino <benimus@gmx.de>
2017-02-03 12:11:21 +0100

    edited whitespaces

--- u11/a02.hs
+++ u11/a02.hs
@@ -3,39 +3,39 @@
 
 --a)
 data MulTree a 	= IndexKnoten a a [MulTree a]
-				| DatenKnoten a
-				deriving Show
+                | DatenKnoten a
+                deriving Show
 
 
 t1 :: MulTree Int
 t1 = IndexKnoten 3 42 [IndexKnoten 3 15 [DatenKnoten 3, DatenKnoten 11, DatenKnoten 12],
-					   IndexKnoten 19 42 [DatenKnoten 42, DatenKnoten 23]
-					  ]
+                        IndexKnoten 19 42 [DatenKnoten 42, DatenKnoten 23]
+                    ]
 --b)
 verzweigunggsgrad :: MulTree a -> Int
 verzweigunggsgrad (DatenKnoten _) = 0
 verzweigunggsgrad (IndexKnoten _ _ xs) = maxInteger (countList xs : makeList xs)
-		where
-			makeList :: [MulTree a] -> [Int]
-			makeList [] = []
-			makeList (x:xs) = countList xs : verzweigunggsgrad x : makeList xs
+        where
+            makeList :: [MulTree a] -> [Int]
+            makeList [] = []
+            makeList (x:xs) = countList xs : verzweigunggsgrad x : makeList xs
 
-			countList :: [MulTree a] -> Int
-			countList []  = 0
-			countList (x:xs) = 1 + countList xs
+            countList :: [MulTree a] -> Int
+            countList []  = 0
+            countList (x:xs) = 1 + countList xs
 
-			maxInteger :: [Int] -> Int
-			maxInteger [] = 0
-			maxInteger (x:xs) = if x > (maxInteger xs) then x else maxInteger xs
+            maxInteger :: [Int] -> Int
+            maxInteger [] = 0
+            maxInteger (x:xs) = if x > (maxInteger xs) then x else maxInteger xs
 
 --c)
 datenListe :: MulTree a -> [a]
 datenListe (DatenKnoten a) = a:[]
 datenListe (IndexKnoten a b xs) = a:b:iterateList xs
-		where
-			iterateList :: [MulTree a] -> [a]
-			iterateList [] = []
-			iterateList (x:xs) = datenListe x ++ iterateList xs
+        where
+            iterateList :: [MulTree a] -> [a]
+            iterateList [] = []
+            iterateList (x:xs) = datenListe x ++ iterateList xs
 
 {--d)
 datenIntervalle :: MulTree Int -> MulTree Int
@@ -46,12 +46,12 @@ datenIntervalle (IndexKnoten a b as) =-}
 contains :: Int -> MulTree Int -> Bool
 contains x (DatenKnoten a) = if a == x then True else False
 contains x (IndexKnoten a b as)	| a > x || x > b = False
-								| a == x || b == x = True
-								| otherwise containshelp x as
-								where
-									containshelp :: Int -> [MulTree Int] -> Bool
-									containshelp x [] = False
-									containshelp x (a:as) = contains x a
+                                | a == x || b == x = True
+                                | otherwise containshelp x as
+                                where
+                                    containshelp :: Int -> [MulTree Int] -> Bool
+                                    containshelp x [] = False
+                                    containshelp x (a:as) = contains x a
 
 {-contains :: Int -> MulTree Int -> Bool
 contains x t = treecontains x (datenListe t) 
